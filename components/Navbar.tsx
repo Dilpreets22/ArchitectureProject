51a0dd1845e06becdeddb7d7f1a6e450a30c8531
Dilpreet <dilpreetss006@gmail.com>
2026-02-14 18:28:31 +0530

    implement homepage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
                         <Box className="logo"/>
 
                         <span className="name">
-                            Roomify
+                            Architex
                         </span>
                     </div>
                     <ul className="links">
